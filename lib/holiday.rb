0cb1ed87db85eab3203f838173a9ad4863146a56
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-18 01:42:40 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.map do |holiday, event|
-if holiday.values == ["BBQ"]
+if holiday.value == ["BBQ"]
   return event
 end
 end
