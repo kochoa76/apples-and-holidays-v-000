79765941dcd3ea525dd57985c21bf01756fde755
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-18 01:30:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
 puts "#{season.capitalize}:"
 holiday_hash.each do |holidays, supplies|
-puts  "#{holidays.to_s.split(" ").map {|w| w.capitalize}.join(" ")}: #{supplies.join(" ")}"
+puts  "#{holidays.to_s.split("_").map {|w| w.capitalize}.join(" ")}: #{supplies.join(",")}"
 end
 end
 
